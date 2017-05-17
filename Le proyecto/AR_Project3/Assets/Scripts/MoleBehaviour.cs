3ba5513c9fb8b3e422d725944850fcea54f52e3d
joeyGumer <pep3553@gmail.com>
2017-05-17 20:49:26 +0200

    Moles rotate, wait and change color

--- Le proyecto/AR_Project3/Assets/Scripts/MoleBehaviour.cs
+++ Le proyecto/AR_Project3/Assets/Scripts/MoleBehaviour.cs
@@ -6,6 +6,19 @@ public class MoleBehaviour : MonoBehaviour {
 
     public float up_time = 5.0f;
     public float speed = 1.0f;
+    public int inactive_min_time = 1;
+    public int inactive_max_time = 10;
+    public int active_time = 3;
+
+    //Materials
+    public Material idle;
+    public Material smashed;
+    public Material special;
+    //
+
+    //Attached GO
+    public GameObject body;
+    public GameObject nose;
 
     delegate void myDelegate();
     myDelegate currentState;
@@ -13,11 +26,25 @@ public class MoleBehaviour : MonoBehaviour {
     public float down_y;
     Vector3 up_position;
     Vector3 down_position;
+    float max_distance;
+    
+
+    System.Random r_generator;
+    public float mole_timer = 0.0f;
+    public float time_to_appear = 0.0f;
+    public bool up = false;
+
+    public float distance;
 
 	// Use this for initialization
 	void Start () {
         up_position = transform.position;
-        down_position = new Vector3(0.0f, down_y, 0.0f);
+        down_position = down_y * transform.up;
+        max_distance = (up_position - down_position).magnitude;
+
+        //Creating random generator
+        string seed = Time.time.ToString();
+        r_generator = new System.Random(seed.GetHashCode());
 
         transform.position = down_position;
 
@@ -30,25 +57,42 @@ public class MoleBehaviour : MonoBehaviour {
 	void Update ()
     {
         currentState();
-	}
+        if (Input.GetKey(KeyCode.A))
+        {
+            body.GetComponent<Renderer>().material = smashed;
+        }
+
+    }
 
     void GoUp()
     {
-        bool ret = false;
+        if (!up)
+        {
+            Vector3 movement = transform.up * speed * Time.deltaTime;
+            transform.Translate(movement);
 
-      
-            float movement = speed * Time.deltaTime;
-            transform.Translate(0.0f, movement, 0.0f);
 
-            if (transform.position.y >= up_position.y)
+            distance = (transform.position - down_position).magnitude;
+
+            if (distance >= max_distance)
             {
                 transform.position = up_position;
-                ret = true;
+                up = true;
+
+                mole_timer = 0.0f;
+                time_to_appear = active_time;
             }
-        
-        if(ret)
+        }
+        else
         {
-            currentState = GoDown;
+            if (mole_timer < active_time)
+            {
+                mole_timer += Time.deltaTime;
+            }
+            else
+            {
+                currentState = GoDown;
+            }
         }
 
 
@@ -62,24 +106,50 @@ public class MoleBehaviour : MonoBehaviour {
     {
         bool ret = false;
 
-        
-            float movement = -speed * Time.deltaTime;
-            transform.Translate(0.0f, movement, 0.0f);
 
-            if (transform.position.y <= down_position.y)
-            {
-                transform.position = down_position;
-                ret = true;
-            }
-        
-            if(ret)
+        Vector3 movement = transform.up * -speed * Time.deltaTime;
+        transform.Translate(movement);
+
+
+        distance = (up_position - transform.position).magnitude;
+
+        if (distance >= max_distance)
         {
+            transform.position = down_position;
+            ret = true;
+        }
+
+        if (ret)
+        {
+            mole_timer = 0.0f;
+            time_to_appear = GetRandom(inactive_min_time, inactive_max_time);
+            
+            currentState = SetNextState;
+        }
+        
+    }
+
+    void SetNextState()
+    {
+
+        if (mole_timer <= time_to_appear)
+        {
+            mole_timer += Time.deltaTime;
+        }
+        else
+        {
+            //Wich rotation will it have
+            float angle = r_generator.Next(0, 360);
+            transform.Rotate(transform.up, angle);
+
+            //Then go up
+            up = false;
             currentState = GoUp;
         }
-      
-            //yield return new WaitForSeconds(up_time);
-            //StopCoroutine(GoUp());
-            //StartCoroutine(GoDown());
-        
+    }
+
+    int GetRandom(int min, int max)
+    {
+        return r_generator.Next(min, max);
     }
 }
