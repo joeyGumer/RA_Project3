61af9b958bbe0f01bc8943a3da27c4bfe433ec66
AlexisCosano <alcoro13@gmail.com>
2017-05-17 21:18:28 +0200

    Main menu first iteration

---
+++ Le proyecto/AR_Project3/Assets/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    void StartGame()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+}
